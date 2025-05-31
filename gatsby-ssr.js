48d338e8be8887c26962dc7f9e2589a0aab64bff
Emma Lane <emma-louise-lane@outlook.com>
2025-05-31 17:32:50 +0100

    Updates to posthog script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,21 +10,12 @@
 
 import React from "react"
 
-const defaultOptions = {
-  isEnabledDevMode: false,
-  apiKey: 'phc_hDENoPr4V0Ic2hdeeMd48j62Y9s98h0U7ALbBWveiHy',
-  apiHost: 'https://app.posthog.com',
-  head: true
-};
-
 export const onRenderBody = (
-  { setHtmlAttributes, setHeadComponents, setPostBodyComponents },
-  pluginOptions
+  { setHtmlAttributes, setHeadComponents },
 ) => {
   setHtmlAttributes({ lang: `en` })
 
-  const options = Object.assign({}, defaultOptions, pluginOptions);
-  const isEnabled = process.env.NODE_ENV === 'production' || options.isEnabledDevMode;
+  const isEnabled = process.env.NODE_ENV !== 'development';
   if (!isEnabled) {
     console.log("PostHog Analytics not enabled");
     return null;
@@ -35,16 +26,18 @@ export const onRenderBody = (
         key={`gatsby-plugin-posthog`}
         dangerouslySetInnerHTML={{
           __html: `
- !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-posthog.init('${defaultOptions.apiKey}', ${JSON.stringify({ api_host: defaultOptions.apiHost, ...defaultOptions.initOptions })})
+            !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init be ys Ss me gs ws capture Ne calculateEventProperties xs register register_once register_for_session unregister unregister_for_session Rs getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSurveysLoaded onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey canRenderSurveyAsync identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty Is ks createPersonProfile Ps bs opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing $s debug Es getPageViewId captureTraceFeedback captureTraceMetric".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+            posthog.init('phc_hDENoPr4V0Ic2hdeeMd48j62Y9s98h0U7ALbBWveiHy', {
+                api_host: 'https://eu.i.posthog.com',
+                defaults: '2025-05-24',
+                person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+            })
           `
-	}}
+	      }}
       />
   );
-  if (options.head) {
-    setHeadComponents([posthogScript]);
-  } else {
-    setPostBodyComponents([posthogScript]);
-  }
+
+  setHeadComponents([posthogScript]);
+
   return null;
 };
