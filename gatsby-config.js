2cded37441ec00892a72cfc887f9fa23ee2a6ce2
Emma Lane <emma-louise-lane@outlook.com>
2025-06-08 00:16:18 +0100

    Feat: Initial addition of badges. Relates to Add badges #9

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     title: `Spark Lane Dev`,
     author: {
       name: `Emma Lane`,
-      summary: `a full stack developer based in the southwest of England; dabbling with different tech stacks and building (hopefully useful) things.`,
+      summary: `a full stack developer based in Somerset; dabbling with different tech stacks and building (hopefully useful) things.`,
     },
     description: ``,
     siteUrl: `https://sparklane.dev/`,
