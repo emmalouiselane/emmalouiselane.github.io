5406dbd223671b64d85ad84b815d75afe3925478
Emma Lane <emma-louise-lane@outlook.com>
2025-09-07 22:03:29 +0100

    Imp Update descriptor, source and layout

--- src/pages/workshop/gaming/stardew-valley.js
+++ src/pages/workshop/gaming/stardew-valley.js
@@ -23,7 +23,7 @@ const StardewValleyTemplate = ({ data, location, pageContext }) => {
     <Layout location={location}>
       <QuickNavComponent /> 
 
-      <Container className="portfolio-item-content">
+      <Container className="gaming-item-content">
         <article>
           <header>
             <h2>
@@ -34,15 +34,19 @@ const StardewValleyTemplate = ({ data, location, pageContext }) => {
           <section>
             <Row>
               <Col>
-                <p>I love Stardew Valley! It's my ultimate cosy game. I can happily spend hours tending my farm, fishing by the river, exploring the mines, or just wandering through Pelican Town. With all the mods I use, there's always something new to discover, which makes it even easier to lose track of time in its charm.</p>
+                <p>
+                  I love Stardew Valley! It's my ultimate cosy game. I can happily spend hours tending my farm, fishing by the river, exploring the mines, or just wandering through Pelican Town. 
+                  With all the mods I use, there's always something new to discover, which makes it even easier to lose track of time in its charm.
+                </p>
+                <p>The resources below are for my own reference and have been added for ease of access - I've either created them, or sourced them elsewhere. Where they've been sourced elsewhere, I've added a note as to the creator/owner.</p>
               </Col>
             </Row>
-
-            
 
             <Tabs defaultActiveKey="museum-layout" fill>
                 <Tab eventKey="museum-layout" title="Museum Layout Reference">
                   <div className="image-layout">
+                    <p>Source: <a href="https://www.reddit.com/r/StardewValleyExpanded/comments/1hfag51/my_museum_layout/" target="_blank">u/FoxgloveAndFireflies</a></p>
+                 
                     {showLargeMuseumLayout && (
                       <StaticImage 
                         src="../../../images/gaming/museum-layout.png" 
@@ -56,9 +60,10 @@ const StardewValleyTemplate = ({ data, location, pageContext }) => {
                         alt="Museum Layout Reference"
                         placeholder="blurred"
                         layout="constrained"
-                        height={500}
+                        height={400}
                       />
                     )}
+                    
                     <Button onClick={() => setShowLargeMuseumLayout(!showLargeMuseumLayout)}>
                       Toggle Image Size
                     </Button>
@@ -66,6 +71,8 @@ const StardewValleyTemplate = ({ data, location, pageContext }) => {
                 </Tab>
                 <Tab eventKey="fish-guide" title="Fish Guide">        
                 <div className="image-layout">
+                    <p>Source: <a href="https://www.reddit.com/r/StardewValley/comments/7rekkf/updated_visual_fish_by_season/" target="_blank">u/Fynzou</a></p>
+
                     {showLargeFishGuide && (
                       <StaticImage 
                         src="../../../images/gaming/fish-guide.png" 
@@ -79,9 +86,10 @@ const StardewValleyTemplate = ({ data, location, pageContext }) => {
                         alt="Fish Guide"
                         placeholder="blurred"
                         layout="constrained"
-                        height={500}
+                        height={400}
                       />
                     )}
+                    <p>Created by me</p>
                     <Button onClick={() => setShowLargeFishGuide(!showLargeFishGuide)}>
                       Toggle Image Size
                     </Button>
