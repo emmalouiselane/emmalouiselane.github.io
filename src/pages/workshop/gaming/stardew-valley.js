308da02f18187c53394a7a255be39890f8c76aaa
Emma Lane <emma-louise-lane@outlook.com>
2025-09-07 21:46:28 +0100

    Imp Image toggle and fish guide

--- src/pages/workshop/gaming/stardew-valley.js
+++ src/pages/workshop/gaming/stardew-valley.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import * as React from "react"
+import { useState } from "react"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../../../components/layout"
@@ -9,9 +10,14 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import QuickNavComponent from "../../../components/quick-nav";
-import { Tabs, Tab } from "react-bootstrap";
+import { Tabs, Tab, Button } from "react-bootstrap";
 
 const StardewValleyTemplate = ({ data, location, pageContext }) => {
+
+  const [showLargeMuseumLayout, setShowLargeMuseumLayout] = useState(false);
+  const [showLargeFishGuide, setShowLargeFishGuide] = useState(false);
+
+
 
   return (
     <Layout location={location}>
@@ -32,15 +38,54 @@ const StardewValleyTemplate = ({ data, location, pageContext }) => {
               </Col>
             </Row>
 
-            <Tabs defaultActiveKey="museum-layout">
-                <Tab eventKey="museum-layout" title="Museum Layout Reference">                    
-                    <StaticImage 
-                      src="../../../images/gaming/museum-layout.png" 
-                      alt="Museum Layout Reference"
-                      placeholder="blurred"
-                      layout="constrained"
-                      height={500}
-                    />
+            
+
+            <Tabs defaultActiveKey="museum-layout" fill>
+                <Tab eventKey="museum-layout" title="Museum Layout Reference">
+                  <div className="image-layout">
+                    {showLargeMuseumLayout && (
+                      <StaticImage 
+                        src="../../../images/gaming/museum-layout.png" 
+                        alt="Museum Layout Reference"
+                        placeholder="blurred"
+                      />
+                    )}
+                    {!showLargeMuseumLayout && (
+                      <StaticImage 
+                        src="../../../images/gaming/museum-layout.png" 
+                        alt="Museum Layout Reference"
+                        placeholder="blurred"
+                        layout="constrained"
+                        height={500}
+                      />
+                    )}
+                    <Button onClick={() => setShowLargeMuseumLayout(!showLargeMuseumLayout)}>
+                      Toggle Image Size
+                    </Button>
+                  </div>
+                </Tab>
+                <Tab eventKey="fish-guide" title="Fish Guide">        
+                <div className="image-layout">
+                    {showLargeFishGuide && (
+                      <StaticImage 
+                        src="../../../images/gaming/fish-guide.png" 
+                        alt="Fish Guide"
+                        placeholder="blurred"
+                      />
+                    )}
+                    {!showLargeFishGuide && (
+                      <StaticImage 
+                        src="../../../images/gaming/fish-guide.png" 
+                        alt="Fish Guide"
+                        placeholder="blurred"
+                        layout="constrained"
+                        height={500}
+                      />
+                    )}
+                    <Button onClick={() => setShowLargeFishGuide(!showLargeFishGuide)}>
+                      Toggle Image Size
+                    </Button>
+                  </div>            
                 </Tab>
             </Tabs>
           </section>
