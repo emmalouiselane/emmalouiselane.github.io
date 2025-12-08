fe903c5ee8beacd5983c30abd4e20072d9473c98
Emma Lane <emma-louise-lane@outlook.com>
2025-12-08 21:55:45 +0000

    IMP Minor homepage content update

--- gatsby-archive/src/components/sign.js
+++ gatsby-archive/src/components/sign.js
@@ -2,18 +2,10 @@
 import * as React from "react"
 
 const SignComponent = ({ content, containerStyle, signStyle }) => {
-  const handleSwingClick = () => {
-    const sign = document.querySelector('.swinging-sign');
-    sign.style.animation = 'none';
-    sign.offsetHeight; // trigger reflow
-    sign.style.animation = 'sway 6s linear forwards';
-  };
-
   return (
       <div className="swinging-sign-container" style={containerStyle}>
         <div className='swinging-sign' 
             style={signStyle}
-            onClick={handleSwingClick}
         >
             <p>{content}</p>
         </div>
